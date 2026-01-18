3ce18c61a0e5f7ca82238c9ec79cd05b08903655
pinank21b <pinankbhayani0@gmail.com>
2026-01-18 11:01:31 +0530

    Add sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://yourdomain.com'
+  const baseUrl = 'https://riyanexims.parvamsoftech.com'
 
   return [
     { url: `${baseUrl}/`, lastModified: new Date() },
